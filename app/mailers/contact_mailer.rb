db9c6f3e9a58083848053bbf3bfed8e55c1c3c80
Johnathan Hewit <johnathan.hewit@gmail.com>
2020-12-29 22:28:05 +0000

    Updated email address.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'johnathan.hewit@outlook.com'
+    default to: 'johnathan.hewit@live.com'
     
     def contact_email(name, email, body)
         @name = name

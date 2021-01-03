e42c792e7e8cd75ba8137b35efd57c039a79a2b8
Johnathan Hewit <johnathan.hewit@gmail.com>
2021-01-03 19:30:49 +0000

    Added user controller and show view.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,7 +20,7 @@ class ProfilesController < ApplicationController
         end
     end
     
-    # Whitelist form fields
+    # Whitelist profile form fields
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description) 
